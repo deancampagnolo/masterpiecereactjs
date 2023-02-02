4352718a149e4a077597ad5d11f3742aad66e365
Dean Campagnolo <deancampagnolo@gmail.com>
2023-02-01 18:19:09 -0800

    Fixed super small linting issue

--- src/Explore/Explore.tsx
+++ src/Explore/Explore.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box} from '@mui/material'
+import { Box } from '@mui/material'
 import { ReactJSXElement } from '@emotion/react/types/jsx-namespace'
 import MPWorkspaceContainer from './Snippet/MPWorkspaceContainer'
 
